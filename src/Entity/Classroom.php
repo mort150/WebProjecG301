46f8bee1678acc10b32b5da9cc82b024968af86d
mort150 <thanhdtgch190775@fpt.edu.vn>
2021-12-30 11:48:38 +0700

    update

--- src/Entity/Classroom.php
+++ src/Entity/Classroom.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\ClassroomRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
+use App\Repository\ClassroomRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity(repositoryClass=ClassroomRepository::class)
